d61bdfc1f0e243243bee0fa237ac2bff8360d25c
Andrii Stepanets <ovrass@gmail.com>
2019-11-20 19:35:33 +0200

    added function working with protitype

--- 04-advance-js/w1-prototypes/index.js
+++ 04-advance-js/w1-prototypes/index.js
@@ -27,3 +27,49 @@ console.log(lecture.getName());
 Object.setPrototypeOf(student, person);
 
 console.log(student.getName());
+
+Object.defineProperty(person, 'age', {
+    set: function (age) {
+        this._age = parseInt(age);
+    },
+    get: function () {
+        return this._age;
+    }
+});
+
+student.age = '20 years';
+console.log(student.age);
+console.log(person.age);
+console.log(student.hasOwnProperty('_age'));
+
+var kitty = {};
+var cat = {};
+
+Object.defineProperty(cat, 'color', {
+    set: function () {
+        this._color = 'Ginger';
+    },
+    get: function () {
+        return this._color;
+    }
+});
+
+Object.setPrototypeOf(kitty, cat);
+kitty.color = 'Grey';
+
+console.log(kitty.color);
+
+
+Object.defineProperty(student, 'number', {
+    enumerable: false,
+    value: 120
+});
+
+for (var key in student) {
+    if (student.hasOwnProperty(key)) {
+        console.log(key);
+    }
+}
+
+
+console.log(Object.keys(student));
