b4a12aa871aa3d040324bb6ca2fe1f95eba29cfa
Andrii Stepanets <ovrass@gmail.com>
2019-11-27 23:28:05 +0200

    Added examples of objects' fabrics

--- 04-advance-js/w2-constructors/index.js
+++ 04-advance-js/w2-constructors/index.js
@@ -68,3 +68,83 @@ var fruitProto = {
 var apple = Object.create(fruitProto);
 
 console.log(apple.isUsefull);
+
+// test relations with objects, constructors and prototypes
+// #1
+var person = {
+    type: 'human',
+    getName: function(){}
+}
+
+var student = {
+    name: 'Billy',
+    [[]]: person
+}
+
+Object.getPrototypeOf(student) === person; // true
+
+//  #2
+
+
+Student.prototype.isPrototypeOf(billy); //true
+Person.prototype.isPrototypeOf(billy); //true
+
+//  #3
+
+billy instanceof Student;  // true
+billy instanceof Person;  // true
+billy instanceof Object;  // true
+
+//  example
+
+var foreverAlone = Object.create(null);
+
+foreverAlone instanceof Object; // false
+console.info(foreverAlone instanceof Object);
+
+// Solving problem of doubling code into constructors
+
+function Person(name) {
+    this.type = 'human',
+    this.name = name
+}
+
+function Student(name) {
+    Person.call(this, name)    
+}
+
+var billy = new Student('Billy');
+
+console.info(billy.name);
+
+// call shaded methog
+
+function Person(name) {
+    this.type = 'human';
+    this.name = name;
+}
+
+Person.prototype.getName = function(){
+    return this.name;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.getName = function() {
+    return 'Student ' + Person.prototype.getName.call(this);
+};
+
+console.info(Student.prototype.getName());
+
+// Constructing objects == object's fabrica
+function fruit() {
+    this.type = type
+}
+var apple = Object.create(fruit, {
+    shape: {value: 'round', writable: false},
+    colour: {value: 'Green'},
+    amount: {writable: true}
+});
+
+apple.amount = 'half';
+
+console.log(apple);
