89c2841956914647563b2ed319fa3c605a808f93
Andrii Stepanets <ovrass@gmail.com>
2019-11-30 00:15:11 +0200

    Solution constructors

--- 04-advance-js/w2-constructors/index.js
+++ 04-advance-js/w2-constructors/index.js
@@ -1,150 +1,63 @@
-'use strict';
+module.exports = Collection;
 
-// example function who create objects
+/**
+ * Конструктор коллекции
+ * @constructor
+ */
+function Collection() {
+    this._arr = [];
+}
+/**
+ * Создание коллекции из массива значений
+ */
 
-var studentProto = {
-    sleep: function(){
-        console.info('zzzZZ ...');
-    }
+Collection.from = function (arr) {
+    return arr.reduce(
+        function (collection, curValue) {
+            collection.append(curValue);
+            return collection;
+        },
+        new Collection()
+    );
 };
 
-function createStudent(name) {
-    var student = {
-        name: name
+
+// Методы коллекции
+Collection.prototype.count = function () {
+    return this._arr.length;
+};
+
+Collection.prototype.values = function () {
+    return this._arr;
+};
+
+Collection.prototype.at = function (position) {
+    return position > 0 && position <= this.count() ?
+        this._arr[--position] :
+        null;
+};
+
+Collection.prototype.append = function (item) {
+    if (item instanceof Collection) {
+        item = item.values();
+    }
+
+    if (item instanceof Array) {
+        Array.prototype.push.apply(this._arr, item);
+    } else {
+        this._arr.push(item);
     };
 
-    Object.setPrototypeOf(student, studentProto);
+    Collection.prototype.removeAt = function (position) {
+        if (this._arr[position - 1]) {
+            this._arr.splice(position - 1, 1);
+            return true;
+        }
 
-    return student;
+        return false;
+    };
 }
 
-var billy = createStudent('Billy');
-var willy = createStudent('Willy');
-
-billy.sleep();
-
-// code constructor 
-
-function Student(name){
-    this.name = name;
-}
-
-var billy = new Student('Billy');
-console.info(billy.name);
-
-// Prototype chains
-
-function Person(){
-    this.type = 'human';
-}
-Person.prototype.getName = function(){
-    return this.name;
-};
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.sleep = function() {
-    console.info('ZZzz...');
-};
-Student.prototype.constructor = Student;
-
-var billy = new Student('Billy');
-
-billy.sleep();
-
-function Lecture(name) {
-    this.name = name;
-}
-Lecture.prototype = Object.create(Person.prototype);
-
-var sergey = new Lecture('Sergey');
-//  sergey.sleep(); This cause problem ----- TypeError: sergey.sleep is not a function
-
-//  Example prototype chain
-
-var fruitProto = {
-    isUsefull: true
-}
-
-var apple = Object.create(fruitProto);
-
-console.log(apple.isUsefull);
-
-// test relations with objects, constructors and prototypes
-// #1
-var person = {
-    type: 'human',
-    getName: function(){}
-}
-
-var student = {
-    name: 'Billy',
-    [[]]: person
-}
-
-Object.getPrototypeOf(student) === person; // true
-
-//  #2
 
 
-Student.prototype.isPrototypeOf(billy); //true
-Person.prototype.isPrototypeOf(billy); //true
 
-//  #3
-
-billy instanceof Student;  // true
-billy instanceof Person;  // true
-billy instanceof Object;  // true
-
-//  example
-
-var foreverAlone = Object.create(null);
-
-foreverAlone instanceof Object; // false
-console.info(foreverAlone instanceof Object);
-
-// Solving problem of doubling code into constructors
-
-function Person(name) {
-    this.type = 'human',
-    this.name = name
-}
-
-function Student(name) {
-    Person.call(this, name)    
-}
-
-var billy = new Student('Billy');
-
-console.info(billy.name);
-
-// call shaded methog
-
-function Person(name) {
-    this.type = 'human';
-    this.name = name;
-}
-
-Person.prototype.getName = function(){
-    return this.name;
-}
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.getName = function() {
-    return 'Student ' + Person.prototype.getName.call(this);
-};
-
-console.info(Student.prototype.getName());
-
-// Constructing objects == object's fabrica
-function fruit() {
-    this.type = type
-}
-var apple = Object.create(fruit, {
-    shape: {value: 'round', writable: false},
-    colour: {value: 'Green'},
-    amount: {writable: true}
-});
-
-apple.amount = 'half';
-
-console.log(apple);
